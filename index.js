c1508430a0e258bc1a7715d5d145f1d96ef25b28
Aysha Muhammed <m.aishat9890@gmail.com>
2021-09-21 14:47:09 +0100

    recent changes

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 let countEl = document.getElementById("count");
+
+
+
 let counter = 0;
 
 function increament(){
-    counter = counter+1;
+    counter +=1;
 
     countEl.innerText = counter;
 
+}
+
+function reset(){
+    document.getElementById("reset");
+    countEl.innerText = 0;
+    counter = 0;
+  
+}
+
+function save(){
+    document.getElementById("save-btn");
+    message=("Your count has been saved!");
+    countEl.innerText= message;
+  
 }
